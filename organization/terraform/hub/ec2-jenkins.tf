cd8f5586becb97d8986f7afd87531b4a2a7e66f4
Ramon C3po <ramonesc3po@gmail.com>
2019-05-07 02:49:04 -0300

    Ajuste Jenkins userdata

--- organization/terraform/hub/ec2-jenkins.tf
+++ organization/terraform/hub/ec2-jenkins.tf
@@ -81,6 +81,17 @@ resource "aws_instance" "ec2_jenkins" {
   #!/bin/bash
   apt-get update
   apt-get install ansible awscli -y
+  if tune2fs -l /dev/xvdf | grep 'jenkinsfiles' 2> /dev/null; then
+    echo "jenkinsfiles already"
+  else
+    y | mkfs.ext4 /dev/xvdf -L jenkinsfiles
+    systemctl stop jenkins && \
+    mkdir /opt/jenkins_tmp && mv -f /var/lib/jenkins/* /var/lib/jenkins/.* /opt/jenkins_tmp && \
+    mount /dev/xvdf /var/lib/jenkins && chown jenkins:jenkins /var/lib/jenkins -R && \
+    shopt -s dotglob && mv -f /opt/jenkins_tmp/* /var/lib/jenkins && rm -rf /opt/jenkins_tmp && \
+    echo "LABEL=jenkinsfiles    /var/lib/jenkins   ext4 defaults,discard    0 0"
+    systemctl start jenkins
+  fi
   EOF
 
   subnet_id = "${element(aws_subnet.public_subnet.*.id, 0)}"
